58f179392e04bd0c0fcdda0e73c25560a8ea64df
aleem <aluddin@redhat.com>
2025-09-26 15:23:07 +0530

    update k8s configs

--- internal/controller/fivetranconnector/controller.go
+++ internal/controller/fivetranconnector/controller.go
@@ -42,9 +42,10 @@ type FivetranConnectorReconciler struct {
 	VaultClient    *vaultpkg.VaultClient
 }
 
-// +kubebuilder:rbac:groups=operator.dataverse.redhat.com,resources=fivetranconnectors,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=operator.dataverse.redhat.com,resources=fivetranconnectors/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=operator.dataverse.redhat.com,resources=fivetranconnectors/finalizers,verbs=update
+// +kubebuilder:rbac:groups=operator.dataverse.redhat.com,namespace=fivetran-operator,resources=fivetranconnectors,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=operator.dataverse.redhat.com,namespace=fivetran-operator,resources=fivetranconnectors/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=operator.dataverse.redhat.com,namespace=fivetran-operator,resources=fivetranconnectors/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",namespace=fivetran-operator,resources=secrets,verbs=get;list;watch
 
 func (r *FivetranConnectorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
